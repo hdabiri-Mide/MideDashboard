757759a4dccb46f827d0a832bb8d2ee61617a178
HamedDabiri_mide <hdabiri@hutchinsoninc.com>
2026-04-15 14:39:28 -0400

    Update sreamlit_app.py

--- sreamlit_app.py
+++ sreamlit_app.py
@@ -288,7 +288,7 @@ if uploaded_file:
                     mode="markers",
                     marker=dict(color="red"),
                     name=f"{ax} Anomaly"
-                # )
+                )
             # st.plotly_chart(fig, width="stretch")
             st.plotly_chart(fig, use_container_width=True)
             st.write("Anomalies detected:", len(anomalies))
