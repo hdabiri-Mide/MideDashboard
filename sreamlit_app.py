188c519180834c46f0e5af186a10aa2771eea417
HamedDabiri <hdabiri@hutchinsoninc.com>
2026-05-01 09:35:54 -0400

    Update sreamlit_app.py

--- sreamlit_app.py
+++ sreamlit_app.py
@@ -781,11 +781,11 @@ col_logo, col_title, col_upload = st.columns([1, 2, 3], gap="large")
 with col_logo:
     st.markdown(
     """
-    <div style="margin-top: 20px;">
+    <div style="margin-top: 30px;">
     """,
     unsafe_allow_html=True
     )
-    st.image("enDAQLOGO.JPG", width=120)
+    st.image("enDAQLOGO.JPG", width=250)
     st.markdown("</div>", unsafe_allow_html=True)
     # st.image("enDAQLOGO.JPG", width=250)
     # st.markdown("</div>", unsafe_allow_html=True)
